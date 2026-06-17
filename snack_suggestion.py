4a37e4c87b0525ed1219239a07ae8ebf43ca471d
ADEBEN5 <johnsonndifon7@gmail.com>
2026-06-17 09:44:11 -0700

    Initial commit

---
+++ snack_suggestion.py
@@ -0,0 +1,14 @@
+# BUILDING A SNACK SYSTEM
+
+snack = input("Enter your preferred snack: ")
+
+print(f"user said: {snack}")
+
+snack = input("Enter your preferred snack: ").lower()
+
+print(f"user said: {snack}")
+
+if snack == "cookies" or snack == "samosa":
+    print(f"Great Choice! We'll serve you {snack}")
+else:
+    print("Sorry, we only serve cookies or samosa with tea")
