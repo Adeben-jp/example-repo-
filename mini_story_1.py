de1a004da09b2e5d9bf7af572de9862111b40261
ADEBEN5 <johnsonndifon7@gmail.com>
2026-06-17 09:35:51 -0700

    Initial commit

---
+++ mini_story_1.py
@@ -0,0 +1,11 @@
+# KETTLE BOILING STORY PROJECT
+
+Kettle_boiled = True
+
+if Kettle_boiled:
+    print("Kettle Done! time to make Chai")
+
+    Kettle_boiled = False
+
+if Kettle_boiled:
+    print("Kettle Done! time to make Chai")
