8d94dcbf15f31c6c25cd0922f050915261c41d88
ADEBEN5 <johnsonndifon7@gmail.com>
2026-06-18 07:06:14 -0700

    Initial commit

---
+++ 09_input_params.py
@@ -0,0 +1,50 @@
+# DESIGN METHODS.
+
+chai = "Ginger chai"
+
+def prepare_chai(order):
+    print("Preparing ", order)
+    
+    
+prepare_chai(chai)
+print(chai)
+
+
+chai = [1, 2, 3]
+
+def edit_chai(cup):
+    cup[1] = 42
+    
+edit_chai(chai)
+print(chai)
+
+
+def make_chai(tea, milk, sugar):
+    print(tea, milk, sugar)
+    
+make_chai("Darjeeling", "Yes", "Low") # positional
+make_chai(tea="Green", sugar="Medium", milk="No") # keywords
+
+
+def special_chai(*ingredients, **extras): # (*ingredients(args), **extras(kwargs))
+    print("Ingredients", ingredients)
+    print("Extras", extras)
+    
+special_chai("Cinnamon", "Cardmon", sweetener="Honey", foam="yes")
+
+def chai_order(order=[]):
+    order.append("Masala")
+    print(order)
+    
+# def chai_order(order=None):
+#     if order is None:
+#         order = []
+        
+        
+def chai_order(order=None):
+    if order is None:
+        order = []
+    print(order)
+    
+chai_order()
+chai_order()
